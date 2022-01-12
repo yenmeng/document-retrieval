14a55380addc66060051ff86f67c6ce025fa2998
Ray Chen <b06901135@ntu.edu.tw>
2022-01-12 19:57:12 +0800

    Update README.md and add test query in dense retrieval

--- dense_retrieval/main.py
+++ dense_retrieval/main.py
@@ -11,9 +11,9 @@ def get_args():
     parser.add_argument('--doc_dir',    type=str, default='dindex-docs-dpr-multi')
     parser.add_argument('--train_dir',  type=str, default='../preprocessed_data/train_query')
     parser.add_argument('--test_dir',   type=str, default='../preprocessed_data/test_query')
-    
-    parser.add_argument('--output_csv', type=str, default='../output/dpr.csv')
-    
+
+    parser.add_argument('--output_dir', type=str, default='../output')
+
     parser.add_argument('-bs', '--batch_size', type=int, default=32)
 
     return parser.parse_args()
@@ -30,37 +30,54 @@ def load_data(data_dir, return_filenames=True):
         return docs, filenames
     else:
         return docs
-    
+
 def retrieve(args):
     train_querys, train_names = load_data(args.train_dir)
+    test_querys, test_names = load_data(args.test_dir)
 
     searcher = SimpleDenseSearcher(
         args.doc_dir, 'facebook/dpr-question_encoder-multiset-base'
     )
-    
-    output = []
-    for i, query in tqdm(enumerate(train_querys)):
+
+    output_train = []
+    print('====== retrieving train ======')
+    for i, query in enumerate(tqdm(train_querys)):
+        # hits = searcher.search(query, k0=50, k=50, alpha=0.1, normalization=True, weight_on_dense=True)
         hits = searcher.search(query, k=50)
 
-        for j in range(50):
-            print(f'{j+1:2} {hits[j].docid:7} {hits[j].score:.5f}')
-        
-        top_k_idx = [hits[k].docid for k in range(50)]
-        output.append((train_names[i], ' '.join(top_k_idx)))
-        
-    return output
+        '''for j in range(50):
+            print(f'{j+1:2} {hits[j].docid:7} {hits[j].score:.5f}')'''
 
-def write_csv(args, output):
-    os.makedirs(os.path.dirname(args.output_csv), exist_ok=True)
-    with open(args.output_csv, 'w') as f:
+        top_k_idx = [hits[k].docid for k in range(50)]
+        output_train.append((train_names[i], ' '.join(top_k_idx)))
+
+    output_test = []
+    print('====== retrieving test ======')
+    for i, query in enumerate(tqdm(test_querys)):
+        # hits = searcher.search(query, k0=50, k=50, alpha=0.1, normalization=True, weight_on_dense=True)
+        hits = searcher.search(query, k=50)
+
+        '''for j in range(50):
+            print(f'{j+1:2} {hits[j].docid:7} {hits[j].score:.5f}')'''
+
+        top_k_idx = [hits[k].docid for k in range(50)]
+        output_test.append((test_names[i], ' '.join(top_k_idx)))
+
+    return output_train, output_test
+
+def write_csv(args, output, mode):
+    os.makedirs(args.output_dir, exist_ok=True)
+    output_csv = os.path.join(args.output_dir, f'dpr_{mode}.csv')
+    with open(output_csv, 'w') as f:
         f.write('topic,doc\n')
-        for item in output:
+        for item in sorted(output, key = lambda tup: int(tup[0])):
             f.write(f'{item[0]},{item[1]}\n')
 
 def main():
     args = get_args()
-    output = retrieve(args)
-    write_csv(args, output)
-    
+    output_train, output_test = retrieve(args)
+    write_csv(args, output_train, 'train')
+    write_csv(args, output_test, 'test')
+
 if __name__ == '__main__':
     main()
--- dense_retrieval/run_dpr.sh
+++ dense_retrieval/run_dpr.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# #preprocess
-# python preprocess.py -i ../preprocessed_data/doc -o ./dpr_docs
-#
-# #encode
-# python -m pyserini.encode input   --corpus  dpr_docs \
-#                           output  --embeddings dindex-docs-dpr-multi --to-faiss \
-#                           encoder --encoder facebook/dpr-ctx_encoder-multiset-base --batch-size 32 \
-#
-# #search
-# python main.py \
-#     --train_dir ../preprocessed_data/train_query \
-#     --test_dir ../preprocessed_data/test_query \
-#     --output_csv ../output/dpr.csv
+#preprocess
+python preprocess.py -i ../preprocessed_data/doc -o ./dpr_docs
+
+#encode
+python -m pyserini.encode input   --corpus  dpr_docs \
+                          output  --embeddings dindex-docs-dpr-multi --to-faiss \
+                          encoder --encoder facebook/dpr-ctx_encoder-multiset-base --batch-size 32 \
+
+#search
+python main.py \
+    --train_dir ../preprocessed_data/train_query \
+    --test_dir ../preprocessed_data/test_query \
+    --output_dir ../output/
 
 #eval
 cd ../
-python eval.py data/train_ans.csv output/dpr.csv
+python eval.py data/train_ans.csv output/dpr_train.csv
